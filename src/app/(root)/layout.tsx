c7eec7fc6fe688f337bb6fc817e69dc0aec68f7e
venkat-katikam <venkat140299@gmail.com>
2024-01-02 00:11:01 +0530

    Added assets and Completed layout for home page i.e completed top, bottom, left and right bars

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Topbar />
+        <main className="flex flex-row">
+          <LeftSidebar />
+          <section className="main-container">
+            <div className="w-full max-w-4xl">{children}</div>
+          </section>
+          <RightSidebar />
+        </main>
+        <Bottombar />
+      </body>
     </html>
   );
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Infohub root page</h1>;
+  return <h1 className="head-text">Infohub root page</h1>;
 }
