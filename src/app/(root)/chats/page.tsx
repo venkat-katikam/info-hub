71dbefbf9428200ff632743074cff4bd9485d3af
venkat-katikam <venkat140299@gmail.com>
2024-04-02 23:49:40 +0530

    Chat feature notification update

--- src/app/(root)/chats/page.tsx
+++ src/app/(root)/chats/page.tsx
@@ -4,7 +4,17 @@ const Page = async () => {
   return (
     <section>
       <h1 className="head-text mb-10 ml-6">Chats</h1>
-      <ChatPage />
+      {/* <ChatPage /> */}
+      <div className="ml-6">
+        <p className="no-result text-left">
+          <div className="flex">
+            <div>Chat feature coming very soon... </div>{" "}
+            <div className="animate-bounce"> ⏳</div>
+          </div>
+          <br></br>Stay connected and chat with friends individually or in
+          groups, right within our app!
+        </p>
+      </div>
     </section>
   );
 };
